0256b0cfea31b1b1a1fc310d5f0ccd71038333e0
blokhinvalentin <blokhin@fortech.dev>
2022-06-28 13:38:57 +0300

    changing back #3

--- src/modules/controllers/task.controller.js
+++ src/modules/controllers/task.controller.js
@@ -29,8 +29,7 @@ createNewTask = (req, res) => {
 changeCheckBoxCheck = (req, res) => {
   try {
     let [_id, isCheck] = [req.body._id, !req.body.isCheck];
-    Task.updateOne({ _id, isCheck }, req.body).then(() => {
-      console.log(isCheck);
+    Task.updateOne({ _id, isCheck }, { isCheck: !isCheck }).then(() => {
       Task.find().sort({ isCheck: 1 }).sort({ creationTime: -1 }).then(allTasks => {
         res.send(allTasks);
       })
@@ -44,8 +43,7 @@ changeCheckBoxCheck = (req, res) => {
 changeTaskTextInfo = (req, res) => {
   try {
     let [_id, text] = [req.body._id, req.body.text];
-    console.log(text);
-    Task.updateOne({ _id, text }, req.body).then(() => {
+    Task.updateOne({ _id }, { text }).then(() => {
       Task.find().sort({ isCheck: 1 }).sort({ creationTime: -1 }).then(allTasks => {
         res.send(allTasks);
       })
