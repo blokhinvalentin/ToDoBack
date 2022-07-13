2abae0090d753d96cd193f6ea77a77b1da978e57
blokhinvalentin <blokhin@fortech.dev>
2022-07-13 17:53:07 +0300

    changing back #17

--- src/modules/controllers/task-controller.js
+++ src/modules/controllers/task-controller.js
@@ -2,11 +2,10 @@ const Task = require('../../models/task');
 
 const getAllTasks = (req, res) => {
   try { 
-    Task.find().sort({ isCheck: 1 }).sort({ creationTime: -1 }).then(result => {
+    Task.find().then(result => {
       res.status(200).send(result);
     })
-  }
-  catch (error) {
+  } catch (error) {
     res.status(400).send("cant get all tasks");
   }
 };
@@ -21,10 +20,9 @@ const createNewTask = (req, res) => {
   const task = new Task({ text, isCheck: false, creationTime: Date.now() });
   try { 
     task.save().then(() => {
-      res.status(400).send(task);
+      res.status(200).send(task);
     });
-  }
-  catch (error) {
+  } catch (error) {
     res.status(400).send("cant create task");
   }
 };
@@ -34,18 +32,21 @@ const changeCheckBoxCheck = (req, res) => {
     const params = req.params;
     const [_id, isCheck] = [params._id, req.body.isCheck];
 
-    if ((!params.hasOwnProperty('_id')) || (_id === '') || (typeof isCheck !== 'boolean')) {
+    if ((!params.hasOwnProperty('_id'))
+    || (_id === '')
+    || (typeof isCheck !== 'boolean')
+    ) {
       throw new Error('id or checkbox are unreachable to read');
     }
 
-    Task.updateOne(
+    Task.findOneAndUpdate(
       { _id }, 
-      { $set: { _id, isCheck } }
-    ).then(() => {
-      getAllTasks(req, res);
+      { $set: { isCheck } },
+      { new: true }
+    ).then(result => {
+      res.status(200).send(result);
     });
-  }
-  catch (error) {
+  } catch (error) {
     res.status(400).send("cant change checkbox");
   }
 }
@@ -55,18 +56,21 @@ const changeTaskTextInfo = (req, res) => {
     const params = req.params;
     const [_id, text] = [params._id, req.body.text];
 
-    if ((!params.hasOwnProperty('_id')) || (_id === '') || (text === '')) {
+    if ((!params.hasOwnProperty('_id'))
+    || (_id === '') 
+    || (text === '')
+    ) {
       throw new Error('id or text are unreachable to read');
     }
 
-    Task.updateOne(
+    Task.findOneAndUpdate(
       { _id },
-      { $set: { _id, text } }
-    ).then(() => {
-      res.status(200).send(text);
+      { $set: { text } },
+      { new: true }
+    ).then(result => {
+      res.status(200).send(result);
     });
-  }
-  catch (error) {
+  } catch (error) {
     res.status(400).send("cant change task");
   }
 };
@@ -83,8 +87,7 @@ const deleteTask = (req, res) => {
     Task.deleteOne({ _id }).then(result => {
       res.status(200).send(result);
     });
-  }
-  catch (error) {
+  } catch (error) {
     res.status(400).send("cant delete task");
   }
 };
